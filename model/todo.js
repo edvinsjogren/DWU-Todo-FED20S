31f351f4356272de31df51b2ab62dbba9e42404c
edvinsjogren <edvin.sjogren@gmail.com>
2021-02-02 21:24:27 +0100

    added routes + model folders

---
+++ model/todo.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const todoSchema = new mongoose.Schema({
+    name: {type: String, required: true, minlength: 2, maxlength: 30},
+    date: {type: Date, default: Date.now}
+})
+
+const Todo = mongoose.model("todo", todoSchema); 
+
+module.exports = Todo; 
