b5c3b67fe1432cb88473507c9ce2b1c6ef93e3bd
edvinsjogren <edvin.sjogren@gmail.com>
2021-02-02 20:08:53 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const router = require("./routes/todoRoute");
+const userRouter = require("./routes/userRoute");
+const app = express();
+require("dotenv").config();
+
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({ extended: false })); 
+
+app.set("view engine", "ejs");
+
+app.use("/", router);
+app.use("/", userRouter);
+
+mongoose.connect(
+  process.env.DATABASE_URL,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) return;
+    app.listen(process.env.PORT || 8000, () => {
+      console.log("Appen körs");
+    });
+  }
+);
