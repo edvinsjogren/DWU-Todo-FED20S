3749346abc92956757b94246914c9a9bf19e392b
edvinsjogren <edvin.sjogren@gmail.com>
2021-02-04 09:04:29 +0100

    quick fixes

--- model/user.js
+++ model/user.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
     name: {type: String, required: true, unique: true},
-    password: {type: String, required: true}
+    password: {type: String, required: true, minlength: 6}
 })
 
 const User = mongoose.model("user", userSchema);
